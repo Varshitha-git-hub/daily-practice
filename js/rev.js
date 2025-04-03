b8b8c6e4dc87174bf55b4ef1e3e6c9f3608ce6fd
Varshitha Meduri <varshithameduri@gmail.com>
2025-04-03 17:04:59 +0530

    Add files via upload

---
+++ js/rev.js
@@ -0,0 +1,106 @@
+// // what is js :-- js is dyanamically typed lang 
+// //            js is high-level pro lan which is used to build functionality
+//   // js is syncronous single threaded lang --> order code line by line xecutes
+//   // js is scripting lang and it sis intrpreter lang
+  
+// // where can i trun my js code ?   in soide browser, outside the browser
+// // in browser :-- we are having browser runtime envrionmenet   :-- js emgime, webapis, event loop, callback , microtask queue
+// // nodejs runtime envirionmenet :-- nodejs  c++ & v8 ryan dahl 2009
+
+// // js engine  :-- it isa  engine which helps us to excute the js code
+// //callstack :-- it is aplce where my js code gets executed
+// // gec :-- global execution context :--  it is a context which carrioes my js code into 2 phases 1.memory 2. code eution
+
+
+// // what is data :-- collection of useful infornmation r set of information like str, numbers etc..
+
+// //how may data types are there? primitive & non-primitive 
+// // primitive :--immutable :-- can t be chnaged r we cat modify them :-- str, num, boolean, null, undefined
+
+// // null vs undefined 
+// // undefined vs not-defined
+
+
+// //non-primitive :-- mutable :-- can be modified 
+// // arrys objects functions
+// let a=[1,1.0,"vamsi",true,false,[1,2,4],undefined, null, {id:1}];
+// console.log(a)
+// a[9]="ravi"
+// a[10]={id:1,name:"vamsi",job:"developer & trainer"}
+// a=[]
+// a.length=10
+// a[3]="ramu"
+// a[8]=200;
+// console.log(a[3],a.length)
+// console.log(a[8],a.length)
+// console.log(a[1],a.length)
+
+
+// const xyz={id:1,name:"vamsi"}
+// xyz.age=26;
+// xyz.job="developer"
+// console.log(xyz)
+// // console.log(a.length)
+// // console.log(a)
+// // var a=10;
+// // // console.log(b)
+
+// // var xyz=100;
+// // console.log(xyz)
+
+
+// operator :-- to perform a sepecific opeartion b/w the 2 opearnds and operation can be arithematic, logicla, comparision
+
+// type of operartors :-- airthematic, logical operators, ternary , assignmenet, compariosion , optional chaininhg etc..
+
+// + - * / 
+// =, += , -=, *=, /=
+// ==, ===, >= ,<=, != , !==, >, <
+//&& , || , ! && ( check both operands are true if tru returns true otherwise false)
+// || (either one operand is truue, it returns tru)
+// ! (it chganges true to false r false to true)
+
+// ternpary operator:--  short syntax for condition state,ment
+// condition ? "" : ""
+// const a=[]===[];
+// const a=[]==[];
+// const a=[]==[1,2,3];
+// const a=[1,2,3]==[1,2,3];
+// let a=10 == 10;
+// a="vamsi"
+// a=""
+// a=" "
+// a="" && ""
+// a=10==="10" && "vamsi"
+// console.log(a ? "true":"false")
+// what are conditional statmenet
+// if ,
+//if else
+// let love=true
+// if(love){
+//  alert("o my god im so happy ,,,,, afet few days only  so unlucky , god pls save me!!!")
+// }else{
+//     alert("god pls dont send me into love")
+// }
+
+
+
+// let c=1==1&&" " || 111==="111" && 0; --> true && true --> true
+// if(c){
+//     alert("treu")
+// }else{
+//     alert("false")
+// }
+
+// true  || false --> true
+// true   && 0 --> false
+
+
+// var vs let
+
+// hoisting 
+
+
+// if-else 
+// if else-if   4 examples
+
